fac6fc3fa467e1a71cc3e90722217d61a4e2308d
Jan Paul <janpaul123@users.noreply.github.com>
2014-08-06 12:41:37 -0700

    Added the legacy-launcher in a subdirectory

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,21 @@
 module.exports = function(config) {
   config.set({
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha', 'chai', 'sinon'],
     files: [
       'bower_components/versal-component-runtime/dist/runtime.min.js',
       'index.html',
       'test/*_spec.js',
       {pattern: 'index.js', included: false},
-      {pattern: 'test/test_gadget.html', included: false}
+      {pattern: 'test/test_gadget.html', included: false},
+
+      // Legacy launcher
+      'bower_components/underscore/underscore.js',
+      'bower_components/backbone/backbone.js',
+      'bower_components/jquery/dist/jquery.js',
+      {pattern: 'legacy-launcher/test_gadget/gadget.css', included: false},
+      'legacy-launcher/mini_require.js',
+      'legacy-launcher/legacy-launcher.js',
+      'legacy-launcher/legacy-launcher_spec.js'
     ],
     reporters: ['dots'],
     port: 9876,
