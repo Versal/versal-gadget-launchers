40e62cabfa801b44e8c43eacb0bc4510c619359c
Jan Paul <janpaul123@users.noreply.github.com>
2014-08-06 12:40:30 -0700

    Use dots reporter instead of progress
    Because of https://github.com/karma-runner/karma/issues/724

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config) {
       {pattern: 'index.js', included: false},
       {pattern: 'test/test_gadget.html', included: false}
     ],
-    reporters: ['progress'],
+    reporters: ['dots'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO, // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
